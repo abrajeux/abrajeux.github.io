3dcd10d426cf04df502b432ce167981ec54f1655
Alexandre Brajeux <brajeux.alexandre@gmail.com>
2019-11-20 14:18:40 +0100

    reset

---
+++ precache-manifest.3808d5aa8059a0666f49625220fc1629.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a55303e2491aebf9a01548906ca72c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "135c738a074c23d2eb7c",
+    "url": "/static/css/main.27a8504f.chunk.css"
+  },
+  {
+    "revision": "7947ffc21e2a5a3dd291",
+    "url": "/static/js/2.506d2b05.chunk.js"
+  },
+  {
+    "revision": "135c738a074c23d2eb7c",
+    "url": "/static/js/main.fb7d6501.chunk.js"
+  },
+  {
+    "revision": "ea1e9d5b994f5fc2cf3d",
+    "url": "/static/js/runtime-main.76a6d3bf.js"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/static/media/1200px-ISO_C++_Logo.svg.f278be79.png"
+  },
+  {
+    "revision": "56f04d34956e54bcf725a10b2fddc908",
+    "url": "/static/media/1200px-React-icon.svg.56f04d34.png"
+  },
+  {
+    "revision": "4eb1819dc863ef386e09d86e9f482a9f",
+    "url": "/static/media/58480979cef1014c0b5e4901.4eb1819d.png"
+  },
+  {
+    "revision": "16e95eacf49fe829ce41c43f568e0a71",
+    "url": "/static/media/GitLab_Logo.svg.16e95eac.png"
+  },
+  {
+    "revision": "0c41eaa55d5265d5b61d7c20d7ec680c",
+    "url": "/static/media/cfLogo.0c41eaa5.png"
+  },
+  {
+    "revision": "af3793ff32565b15f099c7a4a04a01c8",
+    "url": "/static/media/csharp-logo-58C6C6F67A-seeklogo.com.af3793ff.png"
+  },
+  {
+    "revision": "2e26ddd90be9bd8bd4bbaa6411d3c634",
+    "url": "/static/media/diderotLogo.2e26ddd9.png"
+  },
+  {
+    "revision": "1d663ba6d95ec4327dcec610d4dab04d",
+    "url": "/static/media/etnaLogo.1d663ba6.jpeg"
+  },
+  {
+    "revision": "5c5eb75b0b6944785a3d01bc95cb68fc",
+    "url": "/static/media/iconfinder_logo_brand_brands_logos_adobe_2993706.5c5eb75b.png"
+  },
+  {
+    "revision": "dde50b746dc05258972119f5c67a4e20",
+    "url": "/static/media/jdaLogo.dde50b74.jpg"
+  },
+  {
+    "revision": "716fe300cd30c29a43ea82ae60c649b9",
+    "url": "/static/media/logo-2582747_1920.716fe300.png"
+  },
+  {
+    "revision": "0e7d4ad55cdc3db527af6c3d72f41ad0",
+    "url": "/static/media/logo-2582748_1920.0e7d4ad5.png"
+  },
+  {
+    "revision": "7391e3343789939a47c88f77ecb50377",
+    "url": "/static/media/ma_photo.7391e334.png"
+  },
+  {
+    "revision": "d19bbdb4c119b7d984414179907a58d1",
+    "url": "/static/media/pngkey.com-github-icon-png-1787243.d19bbdb4.png"
+  },
+  {
+    "revision": "77a7ef8edf4f59491e15761c46bc3d63",
+    "url": "/static/media/sirapLogo.77a7ef8e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web/html-css/precache-manifest.acbd49b71eb7108381c0425ce495de64.js"
+  "/precache-manifest.3808d5aa8059a0666f49625220fc1629.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web/html-css/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
